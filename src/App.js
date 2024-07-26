07f991752b4910c469309fb592626de5dd0d3c9d
sedica3340 <sedica3340@gmail.com>
2024-07-26 09:18:00 +0900

    리덕스 학습 기본세팅

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
-
-import Counter from './optimizing/components/Counter/Counter';
-import Header from './optimizing/components/Header';
-import { log } from './optimizing/log';
-import ConfigureCounter from './optimizing/components/Counter/ConfigureCounter';
+import ReduxCounter from './redux-practice/components/ReduxCounter';
 
 const App = () => {
-  log('<App /> rendered');
-
   
-  const [chosenCount, setChosenCount] = useState(0);
-
-  const setCountHandler = number => {
-    setChosenCount(number);
-  }
   
 
   return (
     <>
-      <Header />
-      <main>
-        <ConfigureCounter onSet={setCountHandler}/>
-        <Counter key={chosenCount} initialCount={chosenCount} />
-      </main>
+      <ReduxCounter />
     </>
   );
 };
